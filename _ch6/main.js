d9e54199e7da98dc5bcc6d7446773ea9667d806d
felipefsfs <felipe.fonseca.silva@gmail.com>
2020-07-11 18:09:04 -0300

    updated page for ch6

--- _ch6/main.js
+++
@@ -1,9 +0,0 @@
-const CDN = s => `https://cdnjs.cloudflare.com/ajax/libs/${s}`;
-const ramda = CDN('ramda/0.21.0/ramda.min');
-const jquery = CDN('jquery/3.0.0-rc1/jquery.min');
-
-requirejs.config({ paths: { ramda, jquery } });
-requirejs(['jquery', 'ramda'], ($, { compose, curry, map, prop }) => {
-  // app goes here
-
-});
