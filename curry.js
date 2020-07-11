200e30626721c1a64a3ca3f435c387edb1229fdf
felipefsfs <felipe.fonseca.silva@gmail.com>
2020-07-11 15:24:02 -0300

    upd curry & test - allow more args on each call

--- curry.js
+++ curry.js
@@ -7,11 +7,11 @@ function curry(fn, ...args0) {
   return _curry_upt_args(...args0);
   function _curry_upt_args(...args) {
     return _curry;
-    function _curry(new_arg) {
-        if ((args.length + 1) < arity) {
-            return _curry_upt_args(...args, new_arg);
-        }
-        return fn(...args, new_arg);
-    } 
+    function _curry(...new_args) {
+      if ((args.length + new_args.length) < arity) {
+        return _curry_upt_args(...args, ...new_args);
+      }
+      return fn(...args, ...new_args);
+    }
   }
 }
--- curry_test.js
+++ curry_test.js
@@ -1,8 +1,7 @@
 import {
-    assertEquals,
-  } from "https://deno.land/std/testing/asserts.ts"
+  assertEquals,
+} from "https://deno.land/std/testing/asserts.ts";
 import { curry } from "./curry.js";
-
 
 Deno.test("curry 0 args", function () {
   const c = curry(f1);
@@ -12,8 +11,8 @@ Deno.test("curry 0 args", function () {
   function f1() {
     return 10;
   }
-  function f1a(a=9, b=100) {
-    return a + b ;
+  function f1a(a = 9, b = 100) {
+    return a + b;
   }
 });
 
@@ -25,22 +24,23 @@ Deno.test("curry 1 arg", function () {
   function f1(a) {
     return a + 1;
   }
-  function f1a(a, b=100) {
+  function f1a(a, b = 100) {
     return a + b + 1;
   }
 });
 
 Deno.test("curry more args", function () {
   const c = curry(f2);
-  assertEquals(c(10)(9), f2(10,9));
+  assertEquals(c(10)(9), f2(10, 9));
   const ca = curry(f2a);
-  assertEquals(ca(10)(9), f2a(10,9));
+  assertEquals(ca(10)(9), f2a(10, 9));
   const c3 = curry(f3);
-  assertEquals(c3(10)(9)(1), f3(10,9,1));
+  assertEquals(c3(10)(9)(1), f3(10, 9, 1));
+  assertEquals(c3("a")("b")("c"), f3("a", "b", "c"));
   function f2(a, b) {
     return a + b;
   }
-  function f2a(a, b, c=2) {
+  function f2a(a, b, c = 2) {
     return a + b + c;
   }
   function f3(a, b, c) {
